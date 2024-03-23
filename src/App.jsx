da6bd18f39b9c274b720d4797480ef65a7822465
Ankit <ankitmaurya9560@gmail.com>
2024-03-23 10:52:40 +0530

    Firestor

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,51 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import {
-  createUserWithEmailAndPassword,
-  signOut,
-  getAuth,
-  onAuthStateChanged,
-} from "firebase/auth";
+  getFirestore,
+  collection,
+  addDoc,
+  doc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import "../src/App.css";
 import { app } from "./firebase";
-import "./App.css";
-import Signup from "./pages/Signup";
-import SigninPage from "./pages/Sigin";
+import {} from "firebase/firestore";
 
-const auth = getAuth(app);
+const firestore = getFirestore(app);
 
 function App() {
-  const [user, setUser] = useState(null);
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // yes you are logged in
-        setUser(user);
-      } else {
-        // User is signed out
-        // ...
-        setUser(null);
-      }
+  const writeData = async () => {
+    const result = await addDoc(collection(firestore, "users"), {
+      Name: "anupma",
+      Age: 17,
+      isMale: false,
     });
-  }, []);
 
-  if (user === null) {
-    return (
-      <>
-        <h1>Firebase app|| Auth service</h1>
-        <Signup />
-        <SigninPage />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <h1>Hello {user.email}</h1>
-        <button onClick={() => signOut(auth)}>Logout</button>
-      </>
-    );
-  }
+    console.log("Result:", result);
+  };
+
+  const getDocument = async () => {
+    const ref = doc(firestore, "cities", "Ktzi21QeVMbqW81j95kg");
+    const snap = await getDoc(ref);
+    console.log(snap.data());
+  };
+
+  const getDocbyQuery = async () => {
+    const collectionRef = collection(firestore, "users");
+    const q = query(collectionRef, where("isMale", "==", true));
+    const snapshot = await getDocs(q);
+    snapshot.forEach((data) => console.log(data.data()));
+  };
+
+  return (
+    <div>
+      <h1>Firebase FireStore</h1>
+      <button onClick={writeData}>Put Data</button>
+      <button onClick={getDocument}>Get Data</button>
+      <button onClick={getDocbyQuery}>Get Data of Male</button>
+    </div>
+  );
 }
 
 export default App;
